eb83a965bf36f94d5b6066a6041f13b5cab6224d
Pyrotee <pyrotee@gmail.com>
2026-04-09 15:38:30 -0500

    uploading c# exercise 1

---
+++ SE3-C/ejercicio1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            string name, career;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter your career: ");
+            career = Console.ReadLine();
+
+            Console.WriteLine("\n---- Printed with + ----");
+            Console.WriteLine(name + ",Welcome to the Algorithms course" +career);
+
+            Console.WriteLine("\n ---- Printed with $ + ----");
+            Console.WriteLine($"{name}, Welcome to the Algorithms course {career}");
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
