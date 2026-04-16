1ba900419db6f1b940f4a9aa379ea8f0640132e1
Pyrotee <pyrotee@hotmail.com>
2026-04-16 15:55:33 -0500

    updating ejer3

--- SE3-C/ejer3.cs
+++ SE3-C/ejer3.cs
@@ -10,6 +10,15 @@ namespace SE3_C
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter a number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nSuma: " + (num1 + num2));
+            Console.WriteLine("\nResta: " + (num1 - num2));
+            Console.WriteLine("\nMultiplicacion: " + (num1 * num2));
+            Console.WriteLine("\nDivision: " + (num1 / num2));
 
         }
 
---
+++ SE3-P/ejer3.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+print("\nSuma: " , (num1 + num2))
+print("\nResta: " , (num1 - num2))
+print("\nMultiplicacion: " , (num1 * num2))
+print("\nDivision: " , (num1 / num2))
+
