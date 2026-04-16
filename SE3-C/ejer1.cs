e0d44aa51aabe9f94e4aa06562f593acd67a82ca
Pyrotee <pyrotee@hotmail.com>
2026-04-16 15:09:17 -0500

    uploading ejer 1 to py

---
+++ SE3-C/ejer1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    class ejer1
+    {
+    }
+}
---
+++ SE3-P/ejer1.py
@@ -0,0 +1,13 @@
+name = input("Enter name: ")
+career = input("Enter career: ")
+
+print("\n---- Printed with + ----")
+print(name + ",Welcome to the Algorithms course" +career)
+
+print("\n ---- Printed with $ + ----")
+print(f"{name}, Welcome to the Algorithms course {career}")
+
+
+
+
+
